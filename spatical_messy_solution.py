8bf4461c75fadea6cb317c919817dd0166e28df9
sirajismail2001-sys <sirajismail2001@gmail.com>
2025-10-21 14:09:57 +0200

    Create spatical_messy_solution.py

---
+++ spatical_messy_solution.py
@@ -0,0 +1,38 @@
+#### exercise average
+
+f = open("D:/enviournmental programming/P_June2019.asc", "r")
+text= f.read()
+print(text)
+f=open("D:/enviournmental programming/P_June2019.asc", "r")
+for line in f:
+    print (line)
+    lines = f.readlines()
+data_lines = lines[6:]
+data=[]
+ncols= int(f.readlines().split()[1])
+nrows= int(f.readlines().split()[1])
+for i in range (3):
+    f.readlines()
+    
+NA = float (f.readlines().split()[1])
+
+DATA = f.readlines()
+
+data_splitted = []
+for row in DATA:
+    data_splitted.append(row.split())
+
+DATA_float = []
+for row in data_splitted:
+    DATA_float.append([float(i)for i in row])
+
+n = 0
+total = 0
+for i in range (nrows):
+    for j in range(ncols):
+        if DATA_float[i][j]!=NA:
+            total = total+DATA_float[i][j]
+            n = n+1
+Average_P=total/n
+print(Average_P)
+
